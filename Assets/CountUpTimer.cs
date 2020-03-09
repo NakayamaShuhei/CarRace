e3dcee066d56205add54a69cf4a1389a50f4f2ed
中山修平 <hansin464975535629@yahoo.co.jp>
2020-03-09 21:10:02 +0900

    ステージにコライダーセットと車視点で走れるようになった

---
+++ Assets/CountUpTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountUpTimer : MonoBehaviour
+{
+    public static float time;
+
+    void Start()
+    {
+        time = 0f;
+    }
+
+    void Update()
+    {
+        if(Goal.goal == false)
+        {
+            time += Time.deltaTime;
+            float t = Mathf.FloorToInt(time);
+            Text uitext = GetComponent<Text>();
+            uitext.text = "Timer:" + t;
+        }
+    }
+}
